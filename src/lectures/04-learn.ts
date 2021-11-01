9dc0653bacd57bfa9883282fa736466e38eb7338
hoangle <huyhoangle789990@gmail.com>
2021-11-01 15:57:44 +0700

    hetbai

--- src/lectures/04-learn.ts
+++ src/lectures/04-learn.ts
@@ -1,4 +1,28 @@
+import { string } from "yargs";
 
 export function isPositive(n: number){
     return n > 0;
 }
+
+enum Status {
+    PENDING,  //0
+    IN_PROGRESS,//1
+    DONE,//2
+    CANCELLED,//3 
+}
+interface Student {
+    id: number;
+    name: string;
+   }
+   interface List<T> {
+    length: number;
+    // [index: number]: T | string;
+    [index: number]: T ;
+   }
+   const numberList: List<number> = [1, 2, 3];
+//    const numberList: List<number> = [1, 2, 3 ,''];
+   const wordList: List<string> = ['easy', 'frontend'];
+   const studentList: List<Student> = [
+    { id: 1, name: 'easy' },
+    { id: 2, name: 'frontend' },
+   ]
